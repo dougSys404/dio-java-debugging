f2a80fdbf8f0ba5b66ffdc1d5ca7ca72919d9682
Doug <douglas@macOs.local>
2025-05-09 16:14:47 -0300

    Add notes and code fix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Iniciou do programa no método main");
+        a();
+        System.out.println("Finalizou o programa no método main");
+    }
+
+    public static void a(){
+        System.out.println("Entrou no método a.");
+        b();
+        System.out.println("Finalizou o método a");
+    }
+
+    public static void b() {
+        System.out.println("Entrou no método b.");
+
+        for (int i = 0; i <= 4; i++) System.out.println(i);
+        c();
+        System.out.println("Finalizou o método b.");
+    }
+
+    public static void c(){
+        System.out.println("Entrou no método c.");
+        Thread.dumpStack();
+        System.out.println("Finalizou o método c.");
     }
 }
